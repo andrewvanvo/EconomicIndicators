0da3e4c955e8dadfe259e1e1ba51003a9393522b
Andrew Vo <andrew.van.vo@gmail.com>
2023-05-05 23:44:17 -0400

    Yield async working

--- app/src/main/java/com/example/economics/MainActivity.java
+++ app/src/main/java/com/example/economics/MainActivity.java
@@ -7,28 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button federalFundsRateButton;
+    Button treasuryYieldButton;
+    Button consumerPriceIndexButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.federalFundsRateButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        federalFundsRateButton = findViewById(R.id.federalFundsRateButton);
+        federalFundsRateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FedFundsRateService fedFundsRateService = new FedFundsRateService(MainActivity.this);
@@ -41,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String latestFedFundsRate) {
                         Toast.makeText(MainActivity.this, "FFR: "+ latestFedFundsRate + "%", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        treasuryYieldButton = findViewById(R.id.yieldButton);
+        treasuryYieldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                YieldsService yieldsService = new YieldsService(MainActivity.this);
+                yieldsService.getLatestYields(new FedFundsRateService.VolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(MainActivity.this, "Something Broke", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String latestYields) {
+                        Toast.makeText(MainActivity.this, "Yields: "+ latestYields + "%", Toast.LENGTH_SHORT).show();
                     }
                 });
 
---
+++ app/src/main/java/com/example/economics/YieldsService.java
@@ -0,0 +1,61 @@
+package com.example.economics;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class YieldsService {
+    public static final String YIELDS_INTERVAL_MONTHLY_APIKEY_DEMO = "https://www.alphavantage.co/query?function=TREASURY_YIELD&interval=monthly&maturity=10year&apikey=demo";
+    Context context;
+    String latestYields;
+
+    public YieldsService(Context context) {
+        this.context = context;
+    }
+
+    public interface VolleyResponseListener {
+        void onError(String message);
+        void onResponse(String latestYields);
+    }
+
+    public void getLatestYields(FedFundsRateService.VolleyResponseListener volleyResponseListener){
+
+        String url = YIELDS_INTERVAL_MONTHLY_APIKEY_DEMO;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONArray yieldsArray;
+                        JSONObject latestYieldsObject;
+                        latestYields = null;
+                        try {
+                            yieldsArray = response.getJSONArray("data");
+                            latestYieldsObject = yieldsArray.getJSONObject(0);
+                            latestYields = latestYieldsObject.getString("value");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        volleyResponseListener.onResponse(latestYields);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        volleyResponseListener.onError("No Yields Retrieved");
+                    }
+                });
+
+
+        YieldsSingleton.getInstance(context).addToRequestQueue(jsonObjectRequest);
+    }
+
+}
