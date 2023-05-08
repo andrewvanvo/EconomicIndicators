ee3f6340887060994c38ad83ef3e153dbef1a4c3
Andrew Vo <andrew.van.vo@gmail.com>
2023-05-08 13:41:47 -0400

    yields recycler view working

--- app/src/main/java/com/example/economics/activities/TreasuryYieldsActivity.java
+++ app/src/main/java/com/example/economics/activities/TreasuryYieldsActivity.java
@@ -1,6 +1,8 @@
 package com.example.economics.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -8,6 +10,9 @@ import android.widget.Toast;
 import com.example.economics.LoadingDialog;
 import com.example.economics.MainActivity;
 import com.example.economics.R;
+import com.example.economics.adapters.Inflation_CarouselRecyclerViewAdapter;
+import com.example.economics.adapters.Yields_CarouselRecyclerViewAdapter;
+import com.example.economics.models.Econ_Carousel_Model;
 import com.example.economics.services.FedFundsRateService;
 import com.example.economics.services.YieldsService;
 
@@ -15,8 +20,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class TreasuryYieldsActivity extends AppCompatActivity {
     JSONArray dataArray;
+    ArrayList<Econ_Carousel_Model> econCarouselModels = new ArrayList<>();
+
     JSONObject latestYieldsObject;
     String latestYields;
 
@@ -36,19 +45,50 @@ public class TreasuryYieldsActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onResponse(JSONArray yieldsArray) {
-                dataArray = yieldsArray;
-                try {
-                    latestYieldsObject = dataArray.getJSONObject(0);
-                    latestYields = latestYieldsObject.getString("value");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onResponse(JSONArray reversed) {
+                dataArray = reversed;
+
+                //Recyclerview setups
+                RecyclerView carouselRecyclerView = findViewById(R.id.yieldsCarouselRecyclerView);
+                setupCarouselModels(dataArray);
+
+                Yields_CarouselRecyclerViewAdapter adapter = new Yields_CarouselRecyclerViewAdapter(TreasuryYieldsActivity.this, econCarouselModels);
+                carouselRecyclerView.setAdapter(adapter);
+                carouselRecyclerView.setLayoutManager(new LinearLayoutManager(TreasuryYieldsActivity.this, LinearLayoutManager.HORIZONTAL, false));
 
                 loadingDialog.dismissDialog();
-                Toast.makeText(TreasuryYieldsActivity.this, "Yields: "+ latestYields + "%", Toast.LENGTH_SHORT).show();
+
             }
         });
+    }
 
+    private void setupCarouselModels(JSONArray data){
+        String[] carouselDescriptors = {"Latest", "Previous", "Change" };
+        String[] carouselStrings = new String[3];
+        JSONObject latestEconObject;
+        JSONObject prevEconObject;
+        String latestStat;
+        String prevStat;
+        Float delta;
+        String deltaDisplay;
+
+        try {
+            latestEconObject = data.getJSONObject(data.length()-1);
+            prevEconObject = data.getJSONObject(data.length()-2);
+            latestStat = latestEconObject.getString("value");
+            prevStat = prevEconObject.getString("value");
+            delta = Float.parseFloat(latestStat) - Float.parseFloat(prevStat);
+            deltaDisplay = String.format("%.2f", delta);
+
+            carouselStrings[0] = latestStat;
+            carouselStrings[1] = prevStat;
+            carouselStrings[2] = deltaDisplay;
+
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        for (int i =0; i < carouselStrings.length; i++){
+            econCarouselModels.add(new Econ_Carousel_Model(carouselStrings[i],carouselDescriptors[i]));
+        }
     }
 }
---
+++ app/src/main/java/com/example/economics/adapters/Yields_CarouselRecyclerViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.economics.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.economics.R;
+import com.example.economics.models.Econ_Carousel_Model;
+
+import java.util.ArrayList;
+
+public class Yields_CarouselRecyclerViewAdapter extends RecyclerView.Adapter<Yields_CarouselRecyclerViewAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Econ_Carousel_Model> carouselModels;
+
+    public Yields_CarouselRecyclerViewAdapter(Context context, ArrayList<Econ_Carousel_Model> carouselModels) {
+        this.context = context;
+        this.carouselModels = carouselModels;
+    }
+
+    @NonNull
+    @Override
+    public Yields_CarouselRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_yields_carousel_item, parent, false);
+        return new Yields_CarouselRecyclerViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Yields_CarouselRecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.statView.setText(carouselModels.get(position).getStat());
+        holder.descriptorView.setText(carouselModels.get(position).getDescriptor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return carouselModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView statView;
+        TextView descriptorView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            statView = itemView.findViewById(R.id.yieldsStatView);
+            descriptorView = itemView.findViewById(R.id.yieldsStatDescriptorView);
+        }
+    }
+
+}
