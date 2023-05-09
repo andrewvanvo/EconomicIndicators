e8a7fe022b72fe2ac6a56328779caa89fcbb2510
Andrew Vo <andrew.van.vo@gmail.com>
2023-05-09 18:07:59 -0400

    nav drawer working

--- app/src/main/java/com/example/economics/MainActivity.java
+++ app/src/main/java/com/example/economics/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.economics;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,21 +21,65 @@ import com.example.economics.models.EconIndicatorModel;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
     ArrayList<EconIndicatorModel> econIndicatorModel = new ArrayList<>();
     int[] econIndicatorImages = {R.drawable.ic_fed_funds_rate, R.drawable.ic_gdp, R.drawable.ic_inflation,
             R.drawable.ic_yields, R.drawable.ic_unemployment, R.drawable.ic_durable};
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.menuHome:{
+                        Toast.makeText(MainActivity.this, "Menu Home Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.menuOTHER:{
+                        Toast.makeText(MainActivity.this, "Menu Other Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.menuLogout:{
+                        Toast.makeText(MainActivity.this, "Menu Logout Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        //logout button
         Button logoutButton = findViewById(R.id.logout);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,11 +107,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //Drag and drop
+        //Drag and drop functionality
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
         itemTouchHelper.attachToRecyclerView((recyclerView));
 
     }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     private void setUpEconIndicatorModels(){
         String[] econIndicatorNames = getResources().getStringArray(R.array.econ_indicators_titles);
         for (int i = 0; i < econIndicatorNames.length; i++){
