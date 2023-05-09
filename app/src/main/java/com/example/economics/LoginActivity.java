ca806d722bafb7634d53eadd19e0a6f4c48b8cfe
Andrew Vo <andrew.van.vo@gmail.com>
2023-05-09 11:43:59 -0400

    Firebase UI auth implemented

--- app/src/main/java/com/example/economics/LoginActivity.java
+++ app/src/main/java/com/example/economics/LoginActivity.java
@@ -1,14 +1,80 @@
 package com.example.economics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    public static final int RC_SIGN_IN = 1;
+    // creating an auth listener for our Firebase auth
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    // auth options
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.AnonymousBuilder().build()
+            //new AuthUI.IdpConfig.GoogleBuilder().build()
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // if the user is already authenticated then we will
+                    // redirect
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // this method is called when user is not authenticated previously.
+                    startActivityForResult(
+                            // get auth ui instance
+                            AuthUI.getInstance()
+                                    // create our sign in intent
+                                    .createSignInIntentBuilder()
+                                    // diff devices check
+                                    .setIsSmartLockEnabled(false)
+                                    // login options set above
+                                    .setAvailableProviders(providers)
+
+
+                                    //build the login screen
+                                    .build(), RC_SIGN_IN
+                    );
+                }
+            }
+        };
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // we are calling listener method on app resume.
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //remove auth listener method on stop.
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
 }
--- app/src/main/java/com/example/economics/MainActivity.java
+++ app/src/main/java/com/example/economics/MainActivity.java
@@ -6,10 +6,17 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.economics.adapters.recycler_views.Econ_RecylerViewAdapter;
 import com.example.economics.models.EconIndicatorModel;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logoutButton = findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+
+                        //redirecting to login page where our login flow is being displayed.
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(MainActivity.this, "User Signed Out", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(i);
+                            }
+                        });
+            }
+        });
 
         //main recycler view. Set up recycler adapter after models are set up!
         RecyclerView recyclerView = findViewById(R.id.econRecyclerView);
